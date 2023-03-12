fda84ae4e1c2719aac7beb53a771e17f5ac41364
Martin <macamp34@gmail.com>
2023-03-12 20:43:28 +0100

    Improved homepage

--- Glorified-ToDo/Glorified-ToDo/Views/HomePageView.swift
+++ Glorified-ToDo/Glorified-ToDo/Views/HomePageView.swift
@@ -9,34 +9,49 @@ import SwiftUI
 
 struct HomePageView: View {
     let categories: [Category]
+    let tasks: [Task]
     var body: some View {
-        VStack {
-            Text("Welcome Back!")
-                .font(.headline)
-                .multilineTextAlignment(.leading)
-            Spacer()
-            ScrollView(.horizontal) {
-                HStack {
-                    ForEach(categories) { category in
-                        CategoryCardView(category: category)
-                            .frame(width: 200, height: 100)
-                            .background(Color[category.color])
-                            .cornerRadius(20)
-                    }
-                }
-            
-            }
-            Spacer()
+        NavigationView {
             VStack {
+                Rectangle()
+                    .frame(height: 0)
+                    .background(.ultraThinMaterial)
                 
+                ScrollView(.vertical) {
+                    VStack(alignment: .leading) {
+                        Text("Categories")
+                            .foregroundColor(Color.gray)
+                    
+                        ScrollView(.horizontal) {
+                            HStack {
+                                ForEach(categories) { category in
+                                    NavigationLink(destination: CategoryPageView(category: category)) {
+                                        CategoryCardView(category: category)
+                                    }
+                                }
+                            }.padding(.horizontal)
+                        }.padding(EdgeInsets(top: 0, leading: -16, bottom: 0, trailing: -16))
+                        
+                        Text("Tasks for today")
+                            .padding(.top)
+                            .foregroundColor(Color.gray)
+
+                        VStack {
+                            ForEach(tasks) {
+                                task in TaskView(task: task, showDescription: false)
+                            }
+                        }
+                    }
+                    .padding(.all)
+                }
             }
-                
+            .navigationTitle(Text("Overview").font(.title))
         }
     }
 }
 
 struct HomePageView_Previews: PreviewProvider {
     static var previews: some View {
-        HomePageView(categories: Category.sampleData)
+        HomePageView(categories: Category.sampleData, tasks: Task.sampleData)
     }
 }
