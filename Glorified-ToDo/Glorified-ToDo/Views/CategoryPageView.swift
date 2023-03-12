298275b6d4e74bb05190b6588873d22baa5df4f6
Martin <macamp34@gmail.com>
2023-03-12 21:05:45 +0100

    Category page now has proper navigation view

--- Glorified-ToDo/Glorified-ToDo/Views/CategoryPageView.swift
+++ Glorified-ToDo/Glorified-ToDo/Views/CategoryPageView.swift
@@ -11,12 +11,16 @@ struct CategoryPageView: View {
     let category: Category
     
     var body: some View {
-        Text(category.name).font(.title)
-        
-        
-        ForEach(category.tasks) { task in
-            Text(task.name)
+        NavigationView {
+            VStack{
+                Text(category.name).font(.title)
+                
+                ForEach(category.tasks) { task in
+                    Text(task.name)
+                }
+            }
         }
+        .navigationTitle(Text(category.name))
     }
 }
 
